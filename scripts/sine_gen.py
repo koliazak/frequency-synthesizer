144fea918cc271cfb06144ee22747a5e64acc374
koliazak <vrmsumy3@gmail.com>
2026-04-11 20:00:02 +0200

    Added constr file and script to generate LUT for sine

---
+++ scripts/sine_gen.py
@@ -0,0 +1,18 @@
+import math
+
+SAMPLES   = 512
+BIT_WIDTH = 8
+FILENAME  = "sine.mem"
+
+
+MAX_VAL = (2**BIT_WIDTH) - 1
+OFFSET  = MAX_VAL / 2
+AMPLITUDE = MAX_VAL / 2
+
+with open (FILENAME, "w") as f:
+	for i in range(SAMPLES):
+		angle = 2 * math.pi * i / SAMPLES
+		val = int(OFFSET + AMPLITUDE * math.sin(angle))
+		f.write(f"{val}\n")
+
+print(f"File {FILENAME} sussessfully created!")
