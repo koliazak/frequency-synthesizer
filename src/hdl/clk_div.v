9be3cc8844c14dc5da0c681cd0c410f8be18d7b0
koliazak <vrmsumy3@gmail.com>
2026-04-11 20:47:57 +0200

    add initial project structure and clk_div

---
+++ src/hdl/clk_div.v
@@ -0,0 +1,32 @@
+module clk_div 
+#(
+	parameter INPUT_FREQ  = 100_000_000,
+	parameter OUTPUT_FREQ = 48_000
+)
+(
+	input clk_in,
+	input rst_n,
+
+	output reg ce_out
+);
+	
+	localparam div = INPUT_FREQ / OUTPUT_FREQ;
+	
+	reg [$clog2(div)-1:0] counter;
+
+	always @(posedge clk_in) begin
+		if (!rst_n) begin
+			counter <= 0;
+			clk_out <= 0;
+		end else begin
+			if (counter >= (div - 1)) begin
+				counter <= 0;
+				ce_out <= 1;
+			end else begin
+				counter <= counter + 1'b1; 
+				ce_out <= 0;
+			end
+		end
+	end
+
+endmodule
---
+++ src/hdl/top.v
@@ -0,0 +1,8 @@
+module Top(
+	input clock,
+	input reset,
+	input [15:0] SW,
+	output AUD_SD,
+	output AUD_PWM
+);
+endmodule
